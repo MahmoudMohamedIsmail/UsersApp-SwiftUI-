e6ba44ba8996b264c4afb7e50f2bd401c789f76a
mahmoud ismail <mahmoud.ismail2203@gmail.com>
2024-10-08 00:45:18 +0300

    Initial Commit

---
+++ UsersApp/UsersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersAppApp.swift
+//  UsersApp
+//
+//  Created by Mahmoud Ismail on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct UsersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
